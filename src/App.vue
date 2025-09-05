8dbcc5b55fe6ded778f98c6eb0f07aa9be394dff
Tee <tee.banchuen@kobkiat-it.com>
2025-09-05 22:42:19 +0700

    add site

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<script setup></script>
+
+<template>
+  <BCard no-body>
+    <div class="container text-light">
+      <p> <br/></p>
+      <div class="row">
+        <div class="col-lg">
+          <div class="img-fluid logo"><img id="logo" src="./assets/thai-billboard-text-shadow.avif"/></div>
+        </div>
+        <div class="col-lg text-left">
+          <div class="text-center">
+            <h1 style="color: black;">ยินดีครบรอบ 5 ปี Takanashi Kiara</h1>
+            <h2 style="color: black;">Happy 5th Anniversary Takanashi Kiara</h2>
+          </div>
+          <div style="margin-left: 5%;">
+            <br/>
+            <p>
+              โปรเจคป้ายโฆษณา<br/>
+              สถานที่: MBK Center<br/>
+              วันที่ 12-18 กันยายน 2025<br/>
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <div class="row text-center">
+        <h1 style="color: black;">Takanashi Kiara 5th Anniversary Thai Billboard Ad</h1>
+        <div class="video-container">
+          <iframe width="1333" height="750" src="https://www.youtube.com/embed/g5k5yScMWDc" title="TASTY - Takanashi Kiara (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>
+      <hr/>
+      <div class="row text-center">
+        <h1>Credits</h1>
+        <p>
+          Project Managers: NiiNi Diesel<br/>
+          Artist: Yakimi27<br/>
+          Motion graphics, Video Editing, Rigging: acdi33, AnAnomalousWriter and Vini Konbini<br/>
+          Logo Designer: Spiritsnare<br/>
+          Site Engineer: Tee ธี
+        </p>
+        <h1>Special Thanks</h1>
+        <p>
+          HoloFans TH<br/>
+          KFP Thailand Branch<br/>
+          KFP The Office<br/>
+          Kowkarot<br/>
+          PONG<br/>
+          Pudding<br/>
+        </p>
+        <br/>
+        <div class="row" id="creditbox">
+          <h2>Contributors</h2>
+          <small>*Listed in alphabetical order / เรียงตามลำดับตัวอักษร</small>
+          <br/>
+          <div class="col-sm text-center">
+            <span id="credittext">
+              ApiVoynich<br/>
+              Blanket<br/>
+              boo<br/>
+              CannonConoki<br/>
+              Carpe Absurdum<br/>
+              CoppuP<br/>
+              Coryheus<br/>
+              Crayons A. T.<br/>
+              G.D<br/>
+              Garik Garza<br/>
+              Gastornis<br/>
+              Hello"world"<br/>
+              InsulinIngo <br/>
+              Jinro<br/>
+              K3iji<br/>
+              Koiji<br/>
+              Light Aether<br/>
+              Little_Birdcute<br/>
+              lustigkurre<br/>
+              </span>
+            </div>
+            <div class="col-sm text-center">
+              <span id="credittext">
+              MacaronXD<br/>
+              MininiwA<br/>
+              Miru<br/>
+              MR.N<br/>
+              Nechigawara Sanzenin<br/>
+              NiiNi Diesel<br/>
+              Nyfor<br/>
+              P.H.C.<br/>
+              Pannavit<br/>
+              PONG<br/>
+              purekyun<br/>
+              Rei48_jp<br/>
+              Renwah<br/>
+              RIKI<br/>
+              Ryusleep<br/>
+              Satta<br/>
+              Seen<br/>
+              Shinji<br/>
+              SiMurgH<br/>
+              </span>
+            </div>
+            <div class="col-sm text-center">
+              <span id="credittext">
+              Sky<br/>
+              Susu<br/>
+              Tee ธี<br/>
+              Thrud<br/>
+              TK J Sudsap<br/>
+              TK Sudsap<br/>
+              TrewDomko<br/>
+              UNKNOWN<br/>
+              Vinylshark<br/>
+              XNLaizer<br/>
+              Zenas<br/>
+              zer0<br/>
+              ZURG<br/>
+              きのさき【kinosaki】🐔🍍<br/>
+              めらんげ<br/>
+              旅人<br/>
+              นั้นแหละๆ<br/>
+              </span>
+            </div>
+          <p> <br/></p>
+        </div>
+      </div>
+    </div>
+  </BCard>
+</template>
+
+<style scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+small {
+  color: black;
+  /* text-shadow: 2px 2px 4px #000000; */
+  /* text-shadow:
+    -1px -1px 0 rgba(255,255,255, 0.5),
+    1px -1px 0 rgba(255,255,255, 0.5),
+    -1px 1px 0 rgba(255,255,255, 0.5),
+    1px 1px 0 rgba(255,255,255, 0.5); */
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'aos/dist/aos.css'
+
+import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next/plugins/createBootstrap'
+import App from './App.vue'
+import AOS from 'aos'
+
+const app = createApp(App)
+app.use(createBootstrap()) // Important
+app.use(AOS)
+app.mount('#app')
